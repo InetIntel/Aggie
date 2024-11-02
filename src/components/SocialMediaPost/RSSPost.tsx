f0fd8e5b653fb109d44cb22038ce91e64ab46610
last night <fujizhang2525@gmail.com>
2024-11-02 16:22:48 -0400

    better guards for rss metadata rendering

--- src/components/SocialMediaPost/RSSPost.tsx
+++ src/components/SocialMediaPost/RSSPost.tsx
@@ -1,20 +1,38 @@
+import { isObject, isString } from "lodash";
 import { Report } from "../../api/reports/types";
 
 interface IProps {
   report: Report;
 }
 const RSSPost = ({ report }: IProps) => {
-  const rawData = report.metadata.rawAPIResponse as any;
-  const content =
-    "content:encoded" in rawData ? rawData["content:encoded"] : report.content;
+  function renderCategories(item: any) {
+    if (isString(item)) return item;
+    if (isObject(item)) {
+      const category = (item as any)._;
+      if (category) return category;
+      else return "";
+    }
+    return "";
+  }
+
+  function renderContent(raw: any) {
+    if ("content:encoded" in raw) return raw["content:encoded"];
+    if ("content" in raw) return raw["content"];
+    return report.content;
+  }
+
+  const rawData = report?.metadata?.rawAPIResponse as any;
+  const content = renderContent(rawData);
   return (
     <>
       <h2 className='font-medium'>{rawData?.title}</h2>
       <p className=' mb-1'>By {report.author}</p>
       <p className='flex gap-2 flex-wrap text-sm text-slate-700 mb-1'>
         Categories:{" "}
-        {rawData?.categories?.map((i: string) => (
-          <span className='text-slate-800 font-medium bg-slate-100'>{i}</span>
+        {rawData?.categories?.map((i: string, index: number) => (
+          <span key={index} className='text-slate-800 font-medium bg-slate-100'>
+            {renderCategories(i)}
+          </span>
         ))}
       </p>
       <div
