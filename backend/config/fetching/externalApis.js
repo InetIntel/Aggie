dfd9a4230c7c5bd1535131077a641bb9a8819dca
ylw-PotatoTornado <yvwu510@gmail.com>
2025-06-11 17:28:58 -0400

    [UPDATE-config/fetching-utils] Modularize fetching channel configs and utils

---
+++ backend/config/fetching/externalApis.js
@@ -0,0 +1,44 @@
+const API_BASE_URLS = {
+    IODA: process.env.IODA_API_BASE_URL || 'https://api.ioda.inetintel.cc.gatech.edu/v2/',
+    CLOUDFLARE: process.env.GEO_API_BASE_URL || 'https://api.cloudflare.com/client/v4/radar/',
+    
+}
+
+const API_ROUTES = {
+    IODA: {
+        ENTITY_QUERY:'entities/query',
+        OUTAGE_EVENTS:'outages/events',
+    },
+    
+    CLOUDFLARE:{
+        TRAFFIC_ANOMALIES:'traffic_anomalies',
+    }
+}
+
+const DATA_SOURCES = {
+    IODA: {
+        'merit-int': 'Telescope',
+        'ping-slash24': 'Active Probing',
+        'bgp': 'BGP',
+    },
+    CLOUDFLARE: "Cloudflare Traffic"
+}
+
+const API_LINKED_PAGE_URLS = {
+    IODA:{
+        BASE: 'https://ioda.inetintel.cc.gatech.edu',
+    },
+    CLOUDFLARE: {
+        BASE: "https://radar.cloudflare.com",
+        IMAGE_ROUTE: "charts/TrafficTrendsXY/png?image=true"
+    }
+    
+}
+
+module.exports = {
+    API_BASE_URLS,
+    API_ROUTES,
+    DATA_SOURCES,
+    API_LINKED_PAGE_URLS,
+
+}
