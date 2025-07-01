e56eb230eff32ac859520904a244c96448725543
ru1nw <hwen70@gatech.edu>
2025-07-01 14:07:21 -0400

    removed unauthorized elements according to roles

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -11,7 +11,6 @@ import { getSession } from "./api/session";
 import type { AxiosError } from "axios";
 import type { Session } from "./api/session/types";
 
-
 import Navbar from "./Navbar";
 import SourcesIndex from "./pages/Settings/source/SourcesIndex";
 import SourceDetails from "./pages/Settings/source/SourceDetails";
@@ -106,14 +105,20 @@ const PrivateRoutes = ({ sessionData }: IPrivateRouteProps) => {
       <Route path='/settings' element={<Settings />}>
         <Route path='sources' element={<SourcesIndex />} />
         <Route path='source/:id' element={<SourceDetails />} />
-        <Route path='users' element={<UsersIndex session={sessionData} />} />
         <Route
           path='user/:id'
           element={<UserProfile session={sessionData} />}
         />
-        <Route path='tags' element={<TagsIndex />} />
-        { /*<Route path='config' element={<Configuration />} />*/ }
-        <Route path='credentials' element={<CredentialsIndex />} />
+        { sessionData?.role === "admin" &&
+          <>
+            <Route path='users' element={<UsersIndex session={sessionData} />} />
+            <Route path='tags' element={<TagsIndex />} />
+            <Route path='credentials' element={<CredentialsIndex />} />
+          </>
+        }
+        { (sessionData?.role === "admin" || sessionData?.role === "monitor") &&
+          <Route path='tags' element={<TagsIndex />} />
+        }
       </Route>
       <Route path='/*' element={<NotFound />} />
     </Routes>
--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -7,22 +7,40 @@ import {
 
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useQuery } from "@tanstack/react-query";
 import { Link, Outlet, useLocation } from "react-router-dom";
 
-export const menuLinks = {
-  //Config: { to: "config", icon: faCog },
-  "Manage Users": { to: "users", icon: faUsersCog },
-  "Manage Tags": { to: "tags", icon: faTags },
-  "API Credentials": { to: "credentials", icon: faKey },
-  "Manage Sources": { to: "sources", icon: faCloudArrowDown },
-};
+import { getSession } from "../../api/session";
+
+export function menuLinks(role: string | undefined) {
+  switch (role) {
+    case "admin":
+      return {
+        "Manage Users": { to: "users", icon: faUsersCog },
+        "Manage Tags": { to: "tags", icon: faTags },
+        "API Credentials": { to: "credentials", icon: faKey },
+        "Manage Sources": { to: "sources", icon: faCloudArrowDown },
+      };
+    case "monitor":
+      return {
+        "Manage Tags": { to: "tags", icon: faTags },
+        "Manage Sources": { to: "sources", icon: faCloudArrowDown },
+      };
+    case "viewer":
+      return {"Manage Sources": { to: "sources", icon: faCloudArrowDown }};
+    default:
+      return {};
+  }
+}
 
 const Settings = () => {
   const location = useLocation();
+  const { data: session } = useQuery(["session"], getSession);
+
   return (
     <section className='max-w-screen-xl mx-auto w-full grid grid-cols-5 gap-4'>
       <nav className='flex flex-col gap-2 mt-3 pr-3 border-r border-slate-300 min-h-[80vh]'>
-        {Object.entries(menuLinks).map(([name, link]) => (
+        {Object.entries(menuLinks(session?.role)).map(([name, link]) => (
           <Link
             key={name}
             className={`px-3 py-2 grid grid-cols-[16px_1fr] gap-2 items-center font-medium whitespace-nowrap text-left rounded-lg w-full ${location.pathname.includes(link.to)
