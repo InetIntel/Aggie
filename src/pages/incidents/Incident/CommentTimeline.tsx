af11a3416f88763470661a75819b133634f42440
ru1nw <hwen70@gatech.edu>
2025-07-14 17:59:34 -0400

    added 1 file attach to incident comment

--- src/pages/incidents/Incident/CommentTimeline.tsx
+++ src/pages/incidents/Incident/CommentTimeline.tsx
@@ -1,5 +1,6 @@
+import { useCallback, useRef, useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, FieldProps } from "formik";
 import * as Yup from "yup";
 
 import { addComment } from "../../../api/groups";
@@ -15,8 +16,46 @@ import DateTime from "../../../components/DateTime";
 import UserToken from "../../../components/UserToken";
 import Comment from "./Comment";
 
-import { faDotCircle } from "@fortawesome/free-solid-svg-icons";
+import { faDotCircle, faImage } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const ImageUploadField: React.FC<FieldProps> = ({ field, form }) => {
+  const [preview, setPreview] = useState<string | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click()
+  }
+  const handleFileChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      form.setFieldValue(field.name, file);
+
+      const reader = new FileReader();
+      reader.onload = () => setPreview(reader.result as string);
+      reader.readAsDataURL(file);
+    }
+  }, []);
+
+  return <>
+    <AggieButton
+      variant='transparent'
+      icon={faImage}
+      onClick={handleButtonClick}
+      disabled={form.isSubmitting}
+      type='button'
+    >
+      <input
+        type='file'
+        accept='application/pdf, image/jpeg, image/png'
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        className='hidden'
+      />
+    </AggieButton>
+    {preview && <img src={preview} alt="Preview" />}
+  </>;
+};
 
 interface IProps {
   group?: Group;
@@ -30,10 +69,11 @@ const CommentTimeline = ({ group, isLoading }: IProps) => {
   const { data: session } = useQuery(["session"], getSession, {
     staleTime: 50000,
   });
-  function onPostAdd(formData: { commentdata: string }, resetForm: () => void) {
+  function onPostAdd(formData: { commentdata: string, imagedata: null | File }, resetForm: () => void) {
     if (!session || !group) return false;
     const post: EditableGroupComment = {
       data: formData.commentdata,
+      //image: formData.imagedata,
       author: session._id,
     };
     postNewComment.mutate(
@@ -73,18 +113,25 @@ const CommentTimeline = ({ group, isLoading }: IProps) => {
           ))}
       </div>
       <div className=' bg-slate-50 border border-slate-300 rounded-lg'>
-        <h2 className='font-medium ml-3 my-2'>Add Comment</h2>
         <Formik
-          initialValues={{ commentdata: "" }}
+          initialValues={{ commentdata: "", imagedata: null }}
           onSubmit={(e, { resetForm }) => {
             onPostAdd(e, resetForm);
           }}
           validationSchema={Yup.object().shape({
             commentdata: Yup.string().required("Cannot Post Empty Comment!"),
+            imagedata: Yup.mixed(),
           })}
         >
           {({ resetForm, errors }) => (
             <Form>
+              <div className='flex justify-between px-3 py-2'>
+                <h2 className='font-medium'>Add Comment</h2>
+                <Field
+                  name='imagedata'
+                  component={ImageUploadField}
+                />
+              </div>
               <Field
                 as='textarea'
                 name='commentdata'
